bc196a2704437f4dc980600819d5b55e39e29ae6
Erfan Sharafi <erfansharafi60@gmail.com>
2025-04-01 10:52:37 +0330

    feat: home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <div className="px-20">{children}</div>
       </body>
     </html>
   );
