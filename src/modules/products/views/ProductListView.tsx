eeaa24706e91139576d918065b0cbdb4745ffff1
Erfan Sharafi <erfansharafi60@gmail.com>
2025-04-02 11:19:01 +0330

    feat: connect to prisma in .tsx

--- src/modules/products/views/ProductListView.tsx
+++ src/modules/products/views/ProductListView.tsx
@@ -1,7 +1,25 @@
 import ProductList from "@/modules/products/components/ProductList";
 import { DATA } from "@/modules/products/mock/products";
+import { prisma, PrismaType } from "@/lib/prisma";
 
-function ProductListView() {
+async function ProductListView() {
+
+  const data: PrismaType.Product[] = await prisma.product.findMany();
+  console.log(data);
+
+  // const [products, setProducts] = useState<any[]>([]);
+  //
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const result = await prisma.product.findMany({
+  //       include: { images: true },
+  //     });
+  //     setProducts(result);
+  //   };
+  //
+  //   fetchProducts();
+  // }, []);
+
   return (
     <div>
       <ProductList products={DATA} />
